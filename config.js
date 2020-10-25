c336c02e98d504cbd9f5e2b802e19d3a356ba2e3
Alejandro Gómez García <ale.wild.ones@gmail.com>
2020-10-25 13:07:59 -0600

    migrating the project

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
-    authJwtSecret: process.env.AUTH_JWT_SECRET,
-    database: {
-        mongodb: {
-            url: 'mongodb://localhost:27017',
-            dbStaticData: 'FCA',
-            dbSchool: 'school',
-            options: {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }
-        }
-    },
-    urlencoded: {
-        extended: false
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  database: {
+    mongodb: {
+      url: 'mongodb://localhost:27017',
+      dbStaticData: 'FCA',
+      dbSchool: 'school',
+      options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
     }
+  },
+  urlencoded: {
+    extended: false
+  }
 }
--- src/models/DataBase.js
+++ src/models/DataBase.js
@@ -1,37 +1,37 @@
-const MongoClient = require('mongodb').MongoClient,
-      config = require('../../config');
+const MongoClient = require('mongodb').MongoClient
+const config = require('../../config')
 
 /**
  * @class
  */
 class DataBase {
-    /**
-     * 
-     * @returns {Promise<MongoClient>} the mongo client.
-     */
-    static async getClient() {
-        let client = await MongoClient.connect(
-            config.database.mongodb.url, 
-            config.database.mongodb.options
-        );
-        if(!client) throw "We can not make a connection with the mongo database.";
-        return client;
-    }
+  /**
+   * 
+   * @returns {Promise<MongoClient>} the mongo client.
+   */
+  static async getClient () {
+    const client = await MongoClient.connect(
+      config.database.mongodb.url,
+      config.database.mongodb.options
+    )
+    if (!client) throw 'We can not make a connection with the mongo database.'
+    return client
+  }
 
-    /**
-     * @param {String} [db = 'school'] - the database where will be connected 
-     * this client.
-     * @param {String} collection - the name of the collection that will be searched
-     * @returns {Promise<[Collection, MongoClient]>} [collection, client]
-     */
-    static async getCollection(
-        db = config.database.mongodb.dbSchool,
-        collection
-    ) {
-        let client = await DataBase.getClient();
-        let database = client.db(db);
-        return [database.collection(collection), client];
-    }
+  /**
+   * @param {String} [db = 'school'] - the database where will be connected 
+   * this client.
+   * @param {String} collection - the name of the collection that will be searched
+   * @returns {Promise<[Collection, MongoClient]>} [collection, client]
+   */
+  static async getCollection (
+    db = config.database.mongodb.dbSchool,
+    collection
+  ) {
+    const client = await DataBase.getClient()
+    const database = client.db(db)
+    return [database.collection(collection), client]
+  }
 }
 
-module.exports = DataBase;
+module.exports = DataBase
